45c6f92e8088b0f617f6cf844f906c2d320b84ad
Winston Astrachan <winston.astrachan@phobio.com>
2020-02-19 11:28:24 -0500

    Added quick script to grab upstream deb info for tableplus

---
+++ scripts/tableplus/getupstream.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+srcinfo=`curl -s https://deb.tableplus.com/debian/dists/tableplus/main/binary-amd64/Packages`
+echo "${srcinfo}" | grep "Package:"
+echo "${srcinfo}" | grep "Version:"
+echo "${srcinfo}" | grep "Depends:"
+echo "${srcinfo}" | grep "SHA256:"
+echo "${srcinfo}" | grep "Filename:"
